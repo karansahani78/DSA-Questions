3f2bbc7f234f9b14615f2b198a5eed58803ab3d7
karansahani78 <119734907+karansahani78@users.noreply.github.com>
2025-03-08 19:01:44 +0530

    Optimal Solution

---
+++ src/com/SlidingWindow/SArrWithKIntegerOptimal.java
@@ -0,0 +1,41 @@
+package com.SlidingWindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SArrWithKIntegerOptimal {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 1, 2, 3};
+        System.out.println(subarraysWithKDistinct(nums,2));
+        System.out.println(countOfSubarrayWithAtMost(nums,2));
+
+    }
+    public static int subarraysWithKDistinct(int[] nums, int k) {
+        return countOfSubarrayWithAtMost(nums,k)-countOfSubarrayWithAtMost(nums,k-1);
+
+
+    }
+
+    public static int countOfSubarrayWithAtMost(int[] nums, int k) {
+        int count = 0;
+        int start = 0;
+        int end =0;
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        while(end<n){
+            //Expansion phase expanding //adding to the map
+            map.put(nums[end],map.getOrDefault(nums[end],0)+1);
+            // shrinking phase
+            while(map.size()>k){
+                map.put(nums[start],map.get(nums[start])-1);
+                if(map.get(nums[start])==0){
+                    map.remove(nums[start]);
+                }
+                start++;
+            }
+            count = count+(end-start+1);
+            end++;
+        }
+        return count;
+    }
+}
