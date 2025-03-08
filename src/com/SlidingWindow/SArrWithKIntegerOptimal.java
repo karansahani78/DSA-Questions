77f8fb40970864510eea37ed3b599ec6a696cbe9
karansahani78 <119734907+karansahani78@users.noreply.github.com>
2025-03-08 19:05:56 +0530

    Optimal Solution

--- src/com/SlidingWindow/SArrWithKIntegerOptimal.java
+++ src/com/SlidingWindow/SArrWithKIntegerOptimal.java
@@ -6,35 +6,32 @@ import java.util.Map;
 public class SArrWithKIntegerOptimal {
     public static void main(String[] args) {
         int[] nums = {1, 2, 1, 2, 3};
-        System.out.println(subarraysWithKDistinct(nums,2));
-        System.out.println(countOfSubarrayWithAtMost(nums,2));
-
+        System.out.println(subarraysWithKDistinct(nums, 2));
     }
+
     public static int subarraysWithKDistinct(int[] nums, int k) {
-        return countOfSubarrayWithAtMost(nums,k)-countOfSubarrayWithAtMost(nums,k-1);
-
-
+        return countOfSubarrayWithAtMost(nums, k) - countOfSubarrayWithAtMost(nums, k - 1);
     }
 
     public static int countOfSubarrayWithAtMost(int[] nums, int k) {
         int count = 0;
         int start = 0;
-        int end =0;
-        int n = nums.length;
         Map<Integer, Integer> map = new HashMap<>();
-        while(end<n){
-            //Expansion phase expanding //adding to the map
-            map.put(nums[end],map.getOrDefault(nums[end],0)+1);
-            // shrinking phase
-            while(map.size()>k){
-                map.put(nums[start],map.get(nums[start])-1);
-                if(map.get(nums[start])==0){
+        for (int end = 0; end < nums.length; end++) {  // Fixed: starting 'end' from 0 directly
+            // Expansion phase: adding to the map
+            map.put(nums[end], map.getOrDefault(nums[end], 0) + 1);
+
+            // Shrinking phase
+            while (map.size() > k) {
+                map.put(nums[start], map.get(nums[start]) - 1);
+                if (map.get(nums[start]) == 0) {
                     map.remove(nums[start]);
                 }
                 start++;
             }
-            count = count+(end-start+1);
-            end++;
+
+            // Calculating the valid  subarrays count
+            count += (end - start + 1);
         }
         return count;
     }
